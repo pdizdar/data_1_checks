e2cc7b6280d22b50351ef76ecd11fe4ec1c1bc76
Pratima Dizdar <pratima.dizdar@gmail.com>
2022-09-15 00:55:55 -0400

    first commit

---
+++ knowledge_check_1.py
@@ -0,0 +1,9 @@
+print('Hello World!')
+fruits = ['apples', 'bananas', 'cherry', 'grapes', 'oranges' ]
+print(fruits[1])
+
+veggies = {'broccoli' : '3', 'green beans' : '4', 'peppers' : '5'}
+print(veggies['green beans'])
+
+tuple = (1,2,3,4)
+print(tuple[1])
